ff8827d0d7caa7d40aa8d6972c97633ea49a2dd2
杨俊艺 <1209032479@qq.com>
2025-12-19 09:45:18 +0800

    sycn

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
 			"Foo",
 			"Bar",
 		]),
-		.default(enabledTraits: [])// 默认启用 Foo 特性
+		.default(enabledTraits: ["FooBar"])// 默认启用 Foo 特性
 	],
     targets: [
         .target(
